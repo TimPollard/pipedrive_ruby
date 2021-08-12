80e5d7c4be6b5e058de0536a1247de4c3e1a1eb0
Timothy M Pollard <timp@timp.com.au>
2021-08-12 15:04:48 +1000

    Making Persons an actually usuable class.

--- lib/resources/persons.rb
+++ lib/resources/persons.rb
@@ -1,4 +1,18 @@
 # Organizations are companies and other kinds of organizations you are making Deals with.
 # Persons can be associated with organizations so that each organization can contain one or more Persons.
-class Persons
+module PipedriveRuby
+    # This class should hold all endpoints that are specific of deals
+    class Persons < PipedriveRuby::Base
+        def_delegators :endpoints,
+                                  :all,
+                                  :search,
+                                  :find,
+                                  :find_by,
+                                  :create,
+                                  :merge,
+                                  :remove,
+                                  :remove_many,
+                                  :update
+    end
 end
+
